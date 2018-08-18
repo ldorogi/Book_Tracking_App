cdef450a171af43c3a2a5424b3c2d7185c47aeef
László Dorogi <laszlo.dorogi.68@gmail.com>
2018-08-18 23:12:46 +0200

    Adding Book component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-//import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListBooks from "./ListBooks"
 import SearchBooks from "./SearchBooks"
@@ -7,44 +6,6 @@ import {Route, Link} from 'react-router-dom'
 
 
 class BooksApp extends React.Component {
-  /*
-  state = {
-    query: '',
-    books: [],
-    showBooks: []
-  }
-  
-  // get all books before the component is loaded
-  componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({books})
-    })
-  }
-  
-  // input state handling
-  updateQuery = (query) => {
-    this.setState({query: query})
-    let showBooks = []
-    if (query) {
-      BooksAPI.search(query).then(response => {
-        if (response.length) {
-          showBooks = response.map(y => {
-            const index = this.state.books.findIndex(z => z.id === y.id)
-            if( index >= 0 ) {
-              return this.state.books[index]
-            } else {
-              return y
-            }
-          })
-        }
-        this.setState({showBooks})
-      })
-    }
-    else {
-      this.setState({showBooks})
-    }
-  }
-*/
   render() {
     //const {query} = this.state
     return (
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Route, Link} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
+import Book from "./Book";
 
 class SearchBooks extends React.Component {
 	state = {
@@ -58,30 +59,7 @@ class SearchBooks extends React.Component {
             <div className="search-books-results">
               <ol className="books-grid">
                   {this.state.showBooks.map((book, i) => (
-                    <li key={i}>
-                      <div className="book">
-                        <div className="book-top">
-                          <div className="book-cover" style={{
-                            width: 128,
-                            height: 192,
-                            backgroundImage: book.imageLinks ?
-                              `url(${book.imageLinks.thumbnail})` : ''
-                          }}></div>
-                          <div className="book-shelf-changer">
-                            <select onChange={this.changeBookShelf} value={this.state.shelf}>
-                              <option value="move" disabled>Move to...</option>
-                              <option value="currentlyReading">Currently Reading
-                              </option>
-                              <option value="wantToRead">Want to Read</option>
-                              <option value="read">Read</option>
-                              <option value="none">None</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{book.authors ? book.authors.toString() : ' '}</div>
-                      </div>
-                    </li>
+                    <Book key={i} book={book}/>
                   ))}
                 </ol>
             </div>
